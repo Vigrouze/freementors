169c6fb9ed9eeea2c266a9da24698c4251b97f2a
Alex Vigreux <alex.vigreux@kedgebs.com>
2021-11-23 17:36:43 +0100

    Relationship controller created, routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
             only: [:index, :show] do
               resources :missions, only: [:index]
+              resources :relationships, only: [:create]
             end
   resources :missions, only: [:show]
   # no need to nest the mission show as we access by the mission_id
