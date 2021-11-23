153aa7ef9f46ccbcb1520af4307a912eb3f76d26
Alex Vigreux <alex.vigreux@kedgebs.com>
2021-11-23 18:05:16 +0100

    Create method created in Relationships Controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,3 +1,18 @@
 class RelationshipsController < ApplicationController
-  
+  include ApplicationHelper
+
+  def create
+    return if current_user.id == params[:mentor_id]
+    return if relation_request_sent?(User.find(params[:mentor_id]))
+
+    @user = User.find(params[:mentor_id])
+    @relationship = current_user.relation_sent.build(mentor_id: params[:mentor_id])
+      if @relationship.save
+        flash[:sucess] = 'Relation request sent!'
+      else
+        flash[:danger] = 'Relation request failed!'
+      end
+      redirect_back(fallback_location: root_path)
+  end
+
 end
