b5b5138d90f8c3a617a22030d5ad80ea8cdfc724
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-29 10:49:58 +0100

    forgot to delete sender and receiver associations in model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :chatroom
   belongs_to :user
-  belongs_to :sender, class_name: "User", foreign_key: 'sender_id'
-  belongs_to :receiver, class_name: "User", foreign_key: 'receiver_id'
 end
