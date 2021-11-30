42daecc6119f00f890e07b4529d8318ed1b0f8b7
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-30 10:12:29 +0100

    added validation to prevent 2 identical chatrooms to exist

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,6 @@ class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :padawan, class_name: "User"
   belongs_to :mentor, class_name: "User"
+
+  validates :padawan, uniqueness: { scope: :mentor, message: "conversation already created"}
 end
