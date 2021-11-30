1c8ab08e4317cf383733d28ab9e8b9c107ca88ea
Jeremy Blondeau <lebucheronjoe@gmail.com>
2021-11-30 21:08:22 +0100

    javascript

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,16 +1,32 @@
 import { Controller } from "stimulus"
-
+import "jquery-bar-rating";
+import $ from 'jquery';
 
 export default class extends Controller {
-  static targets = ['mentors', 'checkbox'];
+  static targets = ['mentors', 'checkbox', 'select'];
   static values = {
     url: String
   }
 
+  connect() {
+    const that = this;
+    $(this.selectTarget).barrating({
+      theme: 'css-stars',
+      onSelect: function (event) {
+        that.refresh()
+      }
+    });
+  }
+
   refresh(e) {
+    console.log(this.selectTarget.value)
+    // si rating est rempli je viens l'ajouter, sinon je ne rajoute pas
+    // if rating.nil?
+    let rating = `&rating=${this.selectTarget.value}`
+    //
     let checkBoxes = this.checkboxTargets
     const result = checkBoxes.filter(checkbox => checkbox.checked == true).map(checkbox => checkbox.value)
-    const url = `${this.urlValue}?tags=${result}`
+    const url = `${this.urlValue}?tags=${result}${rating}`
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
