598c1d6c084a1a247bb9e9a3eca7396e978d866d
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-12-01 11:58:22 +0100

    simple mentor dashboard created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,9 @@ class PagesController < ApplicationController
   end
 
   def dashboard_mentors
-
+    @user = current_user
+    @missions = @user.missions_as_mentor
+    @accepted_relationships = @user.relationships_as_mentor.where(status: :accepted)
+    @other_relationships = @user.relationships_as_mentor.where.not(status: :accepted)
   end
 end
