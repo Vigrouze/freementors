88af4401a761dcf445d03c2dc26697612135b6b4
Alexandre Havard <alexandrehvrd@gmail.com>
2021-12-02 10:45:13 +0100

    magnificence highlight

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "stimulus";
+import { annotate } from 'rough-notation';
 
 export default class extends Controller {
+  static targets = ['text']
   connect() {
     console.log("Hello from Stimulus")
+    console.log(this.textTarget)
+    const annotation = annotate(this.textTarget, { type: 'highlight', strokeWidth: 3, color: '#fea93a', animationDuration: 1250 });
+    annotation.show();
   }
 }
