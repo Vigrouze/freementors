23f58fa143f27a5eaf057f4c212519ce8fb996c3
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-12-02 16:30:07 +0100

    no validations on the motivation for apply

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,7 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   updateNavbar() {
-    if (window.scrollY >= 100) {
+    const page = window.location.pathname
+    let threshold
+    if (page == '/dashboard') {
+      threshold = 0
+    } else {
+      threshold = 100
+    }
+    if (window.scrollY >= threshold) {
       this.element.classList.add('white-bg');
     } else {
       this.element.classList.remove('white-bg');
